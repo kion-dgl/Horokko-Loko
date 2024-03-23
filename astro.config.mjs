e4560876e411d53ee69016124934ce018d0e4216
kion <kion@dashgl.com>
2024-03-24 03:14:56 +0900

    remove qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import qwikdev from "@qwikdev/astro";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), qwikdev(), icon()]
+  integrations: [tailwind(), icon()]
 });
