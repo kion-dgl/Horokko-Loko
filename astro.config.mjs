f4eabb6323819ca71c56b73bbac05dbd802f2970
Kion <kion@dashgl.com>
2024-03-28 14:17:13 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dashgl.github.io',
-  base: '/horokko-loko',
+  base: '/Horokko-Loko',
   integrations: [tailwind(), icon(), react()]
 });
